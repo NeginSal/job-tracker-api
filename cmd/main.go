14ca0cb3b4c99a550fa61104cffa45ccb7f6c48e
NeginSal <neginsaljooghi@gmail.com>
2025-07-15 16:59:22 +0330

    feat: add application entry point with user registration route

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/NeginSal/job-tracker-api/internal/user"
+	"github.com/NeginSal/job-tracker-api/pkg/db"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"job-tracker-api/internal/user"
+	"job-tracker-api/pkg/db"
 )
 
 func main() {
-	fmt.Println("🚀 Job Tracker API is starting...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("can not load .env file")
+	}
+
+	database := db.NewPostgresConnection()
+	database.AutoMigrate(&user.User{})
+
+	userRepo := user.NewRepository(database)
+	userService := user.NewService(userRepo)
+	userHandler := *user.NewHandler(userService)
+
+	r := gin.Default()
+	r.POST("/register", userHandler.Register)
+
+	log.Println("Server running at http://localhost:8080")
+	r.Run(":8080")
 }
